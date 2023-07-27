20661f68113d6d190d63d05fca7ffe43d99145bb
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-07-27 23:22:08 +0700

    fix back action without config from activity

--- app/src/main/java/com/jonrysimbolonstory/fragment/authentication/AuthenticationFragment.kt
+++ app/src/main/java/com/jonrysimbolonstory/fragment/authentication/AuthenticationFragment.kt
@@ -25,7 +25,7 @@ class AuthenticationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        (activity as AppCompatActivity).supportActionBar?.hide()
         binding.apply {
             val sectionsPagerAdapter = SectionsPagerAdapter(requireActivity() as AppCompatActivity)
             viewPager.adapter = sectionsPagerAdapter
